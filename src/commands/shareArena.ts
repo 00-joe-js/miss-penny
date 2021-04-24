11168a126bc1edd5a0d8cddf8cdaf31131638623
Joe Alves <joseph.michael.alves@gmail.com>
2021-04-23 22:27:38 -0400

    Inactive

--- src/commands/shareArena.ts
+++ src/commands/shareArena.ts
@@ -1,5 +1,6 @@
-import { code, password } from "../../sens/arena-right-now.json";
+import { active, code, password } from "../../sens/arena-right-now.json";
 
 export default (): string => {
+    if (!active) return `Sorry, no public arena right now!`;
     return `Join me in the arena! Code: ${code}; Password: ${password}`;
 };
