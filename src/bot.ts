94d5e40c4ceb93be64b8ef3b7e8467581a6d4989
Joe Alves <joseph.michael.alves@gmail.com>
2021-04-23 22:24:14 -0400

    Basic !arena functionality NEED TO VERIFY FOR WHISPERING

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,63 @@
-import type { ChatUserstate } from "tmi.js";
+import { ChatUserstate, client } from "tmi.js";
 
 import { Client } from "tmi.js";
 import { twitchToken, username, channels } from "../sens/creds.json";
 
 import smashRandom from "./commands/smashRandom";
+import shareArena from "./commands/shareArena";
+
+const commands: { [k: string]: (opts: any, userState: ChatUserstate) => void } = {
+    "!smashRandom": (opts, userState) => {
+        const selection = smashRandom(opts);
+        twitchChatBotClient.say(channels[0], selection);
+    },
+    "!arena": (opts, userState) => {
+        const message = shareArena();
+        if (userState.username) {
+            // TODO: get bot account verified with twitch in order to whisper
+            twitchChatBotClient.say(userState.username, message);
+        }
+    }
+};
+
+const spaces = /\s/gi;
+const parseParameters = (messageText: string) => {
+    messageText = messageText.replace(spaces, "");
+    const openParenPos = messageText.indexOf("(");
+    if (openParenPos === -1) return {};
+    const closeParenPos = messageText.indexOf(")");
+    const parametersString = messageText.slice(openParenPos + 1, closeParenPos);
+    return parametersString.split(",").reduce((o: { [key: string]: string }, s) => {
+        const [key, value] = s.split("=");
+        if (key && value) {
+            o[key.trim()] = value.trim();
+        }
+        return o;
+    }, {});
+};
+const parseMessageToCommandRunner = (messageText: string, userState: ChatUserstate): Function | null => {
+    if (messageText[0] !== "!") return null;
+    const firstPart = messageText.split("(")[0];
+    if (!commands[firstPart]) {
+        return null;
+    } else {
+        const parameters = parseParameters(messageText);
+        return commands[firstPart].bind(null, parameters, userState);
+    }
+};
+
+const handleMessage = async (channel: string, userState: ChatUserstate, message: string, self: boolean) => {
+    if (channel !== channels[0]) return; // Defense
+    if (self === true) return; // Do not handle messages sent from the bot.
+    try {
+        const command = parseMessageToCommandRunner(message.trim(), userState);
+        if (command !== null) {
+            command();
+        }
+    } catch (e) {
+        console.error(e);
+    }
+};
 
 const twitchChatBotClient = new Client({
     identity: {
@@ -16,46 +70,6 @@ const twitchChatBotClient = new Client({
 twitchChatBotClient.on("connected", () => {
     twitchChatBotClient.on("message", handleMessage);
 });
-
-const commands: { [k: string]: (opts: any) => string } = {
-    "!smashRandom": smashRandom
-};
-
-const spaces = /\s/gi;
-const parseParameters = (messageText: string) => {
-    messageText = messageText.replace(spaces, "");
-    const openParenPos = messageText.indexOf("(");
-    const closeParenPos = messageText.indexOf(")");
-    const parametersString = messageText.slice(openParenPos + 1, closeParenPos);
-    return parametersString.split(",").reduce((o: { [key: string]: string }, s) => {
-        const [key, value] = s.split("=");
-        o[key] = value;
-        return o;
-    }, {});
-};
-
-const parseMessageToCommand = (messageText: string): Function | null => {
-    if (messageText[0] !== "!") return null;
-    const firstPart = messageText.split("(")[0];
-    if (!commands[firstPart]) {
-        return null;
-    } else {
-        const parameters = parseParameters(messageText);
-        return commands[firstPart].bind(null, parameters);
-    }
-};
-
-const handleMessage = async (channel: string, userstate: ChatUserstate, message: string, self: boolean) => {
-    if (self === true) return; // Do not handle messages sent from the bot.
-    try {
-        const command = parseMessageToCommand(message.trim());
-        if (command !== null) {
-            twitchChatBotClient.say("#joe_js", command());
-        }
-    } catch (e) {
-        console.error(e);
-    }
-};
 
 (async () => {
     try {
---
+++ src/commands/shareArena.ts
@@ -0,0 +1,5 @@
+import { code, password } from "../../sens/arena-right-now.json";
+
+export default (): string => {
+    return `Join me in the arena! Code: ${code}; Password: ${password}`;
+};
