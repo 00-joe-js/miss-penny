64c0a39a37dcd9ca544025ca165f33c5c76fde48
Joe Alves <joseph.michael.alves@gmail.com>
2021-04-28 21:19:32 -0400

    SSL journey

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
-import { PORT } from "../sens/env.json";
+import { NODE_ENV, PORT, SSL_DIR, SSL_BUNDLE, SSL_CERT, SSL_KEY, SERVER_HOSTNAME } from "../sens/env.json";
 import express from "express";
 import establishSSLVerificationRoutes from "./sslVerification/index";
 
+import {readFileSync} from "fs";
+import {join} from "path";
+
 const app = express();
-establishSSLVerificationRoutes(app);
 
 app.get("/", (_, res) => res.send("Brb, gettin biscuits"));
 
-app.listen(PORT, () => console.log(`goodness me you have a server on ${PORT}`));
+import https from "https";
+if (NODE_ENV === "production") {
+
+    if (!SSL_DIR) throw new Error("No SSL directory specified.");
+
+    const getSSLPath = (path: string) => join(SSL_DIR, path);
+    const sslOptions = {
+        cert: readFileSync(getSSLPath(SSL_CERT)),
+        ca: readFileSync(getSSLPath(SSL_BUNDLE)),
+        key: readFileSync(getSSLPath(SSL_KEY))
+    };
+
+    const server = https.createServer(sslOptions);
+    server.on("request", app);
+    server.listen(PORT, SERVER_HOSTNAME);
+
+    establishSSLVerificationRoutes(app);
+} else {
+    app.listen(PORT, () => console.log(`Here for you on ${PORT}`));
+}
