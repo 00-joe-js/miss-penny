12b258ed18578bcea356bb94311321644b52f640
Joe Alves <joseph.michael.alves@gmail.com>
2021-05-03 19:04:47 -0400

    ok typescript

--- client-src/src/twitchAuth/index.tsx
+++ client-src/src/twitchAuth/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const twitchFirstStopURL = `https://id.twitch.tv/oauth2/authorize\
 ?client_id=quwrspx68engp5hv7b8ush3vn00zej\
-&redirect_uri=${location.origin}/biscuit\
+&redirect_uri=${window.location.origin}/biscuit\
 &response_type=code`;
 
 export const askForTwitchUserInfo = async () => {
